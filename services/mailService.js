815283cc4a4d3baffa0ac4468b6ad328c99b5a8a
Zuev-Yaroslav <zuevyaroslav937@gmail.com>
2023-09-21 10:26:15 +0300

    2114

--- services/mailService.js
+++ services/mailService.js
@@ -4,15 +4,34 @@ dotenv.config()
 
 class MailService {
     constructor() {
-        this.transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: true,
-            auth: {
-                user: process.env.SMTP_USER,
-                pass: process.env.SMTP_PASSWORD
+        var mailConfig;
+        if(process.env.NODE_ENV === 'production'){
+            mailConfig = {
+                host: process.env.SMTP_HOST,
+                port: process.env.SMTP_PORT,
+                secure: true,
+                tls: {
+                    servername: "purple-test.ru",
+                    rejectUnauthorized: true,
+                    minVersion: "TLSv1.2"
+                },
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD
+                }
             }
-        })
+        } else {
+            mailConfig = {
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false,
+                auth: {
+                    user: "edmund.deckow37@ethereal.email",
+                    pass: "8B2mJqbB6t1TCNUxjg"
+                }
+            }
+        }
+        this.transporter = nodemailer.createTransport(mailConfig)
     }
     async sendActivation(to, otp) {
         await this.transporter.sendMail({
